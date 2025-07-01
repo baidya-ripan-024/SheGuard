07b41c9fb47ad5215a3e4cb37079a5ef3bd8fc6f
Ripan Baidya <ripanbaidya7980@gmail.com>
2025-07-01 20:22:13 +0530

    added microservices

---
+++ notification-service/src/test/java/com/sheguard/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheguard.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
